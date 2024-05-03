4994486aeed747271f7981066867146c13d7b636
keisenSusanto <165564174+keisenSusanto@users.noreply.github.com>
2024-05-03 13:26:26 +0700

    Buat kamu

---
+++ src/desktop1/desktop1.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './desktop1.css';
+
+function Desktop1() {
+    return(
+        <div className="desktop1">
+            <h1>Home</h1>
+            <br />
+            <h1>Bali Nusantara</h1>
+            <img src="#"></img>
+                <div className="footer">
+                    <img src="#"></img>
+                    <p>Bali, pulau surga di Indonesia, tidak hanya menawarkan keindahan alam yang memukau tetapi juga menyajikan warisan kuliner yang kaya dan lezat. Dari Babi Guling yang terkenal hingga Ayam Betutu yang meriah dengan rempah-rempah khas Bali, setiap hidangan mencerminkan kekayaan budaya dan tradisi lokal. Rasakanlah sensasi Lawar yang khas dengan daging cincang dan rempah, atau nikmati kelezatan Sate Lilit yang dipanggang dengan cita rasa harum rempah-rempah. Bali tidak hanya memanjakan mata dengan keindahan alamnya, tetapi juga menyajikan pengalaman kuliner yang tak terlupakan bagi para pengunjungnya.
+                    </p>
+                </div>
+        </div>
+    )
+}
+
+export default Desktop1;
---
+++ src/desktop3/desktop3.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Desktop1() {
+    return(
+        <div className="desktop1">
+            <h1>Home</h1>
+        </div>
+    )
+}
