5a7b4747dabad4db2139a623d1e9f97c7474ff36
keisenSusanto <165564174+keisenSusanto@users.noreply.github.com>
2024-05-12 16:51:29 +0700

    Update Styling + Update Desc3

--- src/desktop3/Desk3.js
+++ src/desktop3/Desk3.js
@@ -1,44 +1,42 @@
 import './Desk3.css';
-import  DescComp from './descComp';
+import DescComp from './descComp';
 import imageList from '../desktop2/images';
-import {useState, useEffect} from "react";
-import {useParams} from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import useFoodFetch from "./components/foodList";
-import {Link} from "react-router-dom";
+import Belakang from "./batu.jpg";
 
 function Desk3() {
-  const [   currFood, setcurrFood] = useState({});
-  let par =  useParams();
+  const [currFood, setcurrFood] = useState({});
+  let par = useParams();
   let id = Number(par.id);
-  const { foodF : foodList, loading, error } = useFoodFetch()
+  const { foodF: foodList, loading, error } = useFoodFetch()
   const [index, setIndx] = useState(id);
 
-    useEffect(() => {
-        if(foodList.length > 0){
-            setcurrFood(foodList.find(food => {
-                return Number(food.id) === index;
-            }));
-
-        }
-    }, [foodList, index]);
-
-    function subIndex(){
-        setIndx((index + 11) % 12);
+  useEffect(() => {
+    if (foodList.length > 0) {
+      setcurrFood(foodList.find(food => {
+        return Number(food.id) === index;
+      }));
     }
+  }, [foodList, index]);
 
-    function addIndex(){
-        setIndx((index + 13) % 12);
-    }
+  function subIndex() {
+    setIndx((index + 11) % 12);
+  }
 
+  function addIndex() {
+    setIndx((index + 13) % 12);
+  }
 
-    return (
-      <div className="foodDetails">
-          {foodList && <DescComp name={currFood.name} link={imageList[index]} desc={currFood.description}/>}
-          <div className="d-flex flex-column align-items-center">
-              <Link to={`/foods/${(id + 11) % 12}`} className="btn-support" onClick={subIndex}>Prev</Link>
-              <Link to={`/foods/${(id + 13) % 12}`} className=" btn-support" onClick={addIndex}>Next</Link>
-          </div>
+  return (
+    <div className="foodDetails">
+      {foodList && <DescComp name={currFood.name} link={imageList[index]} desc={currFood.description} />}
+      <div className="d-flex justify-content-center tombol">
+        <a href={`/foods/${(id + 11) % 12}`} className="bn62 left" onClick={subIndex}><span className="bn31span">Prev</span></a>
+        <a href={`/foods/${(id + 13) % 12}`} className="bn62 right" onClick={addIndex}><span className="bn31span">Next</span></a>
       </div>
+    </div>
   );
 }
 
