44724e1286655420f851da1958920b6dcb7581fe
gr1c <richiegiansanto@gmail.com>
2024-05-13 18:06:09 +0700

    push label

--- src/desktop2/Card.js
+++ src/desktop2/Card.js
@@ -1,19 +1,18 @@
-import React from 'react';
+import React from "react";
 import "./desktop2.css";
-import {Link}  from "react-router-dom";
+import { Link } from "react-router-dom";
 
-function Card({ id, link, name, tag, rating}) {
+function Card({ id, link, name, tag, rating }) {
   return (
-      <div key={id}>
-        <Link to={`/foods/${id}`} className="bg">
-          <div className="card">
+    <div key={id}>
+      <Link to={`/foods/${id}`} className="bg">
+        <div className="card">
           <h2 className="name">{name}</h2>
-          {tag && <h2 className="tag">{tag}</h2>}
-          <img src={link} alt={name}></img>
-          <div>{rating}</div>
-          </div>
-        </Link>
-      </div>
+          {tag && <p className="tagtext">{tag}</p>}
+          <img className="imageCard" src={link} alt={name}></img>
+        </div>
+      </Link>
+    </div>
   );
 }
 
